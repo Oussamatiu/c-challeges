416585faac7a6214d56c9deb566c61bf4c9d5844
oussama <oussamachentouf864@gmail.com>
2025-09-23 17:40:26 +0100

    faire de challenges de conditons

---
+++ day1/conditions/challenge 8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  float note;
+  printf("entre la note : ");
+  scanf("%f", &note);
+  if(note < 10 ){
+    printf("recale");
+  }else if(note >= 10 && note < 12){
+    printf("passable");
+  }else if(note >= 12 && note < 14){
+    printf("assez bien");
+  }else if(note >= 14 && note < 16){
+    printf("bien");
+  }else if (note >= 16 && note <= 20){
+    printf("tres bien");
+  }else printf("error");
+  
+ 
+    
+  return 0;
+}
