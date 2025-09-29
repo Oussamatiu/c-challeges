e73661e66bb7917c3a0c28533f4d0453c6cbd42a
oussama <oussamachentouf864@gmail.com>
2025-09-29 17:27:19 +0100

    mini projet

--- day3/mini projet.c
+++ day3/mini projet.c
@@ -4,40 +4,49 @@
 struct Contact
 {
     char nom[30];
-    int numero [10];
+    char numero [10];
     char email[200];
 };
 
 int main(){
     int choix , count=0 ;
     char name[30];
+    char newEmail[200] ;
+    char newNumero[10];
+    
     struct Contact contacts[MAX];
+
     
     
-    printf("===menu===\n1.ajoute un contact\n2.modifier un contact\n3.Supprimer un Contact\n4.Afficher Tous les Contacts\n5.Rechercher un Contact\n6.quitter\nvoutre choix : ");
-    scanf("%d", &choix);
+    
     do
     {
         printf("===menu===\n1.ajoute un contact\n2.modifier un contact\n3.Supprimer un Contact\n4.Afficher Tous les Contacts\n5.Rechercher un Contact\n6.quitter\nvoutre choix : ");
         scanf("%d", &choix);
+        getchar();
         switch (choix)
         {
         case 1:
-            printf("donner le nom : ");
-            fgets(contacts->nom[count] ,sizeof(contacts->nom[count]),stdin);
-            printf("donner le numero : ");
-            fgets(contacts->numero[count] ,sizeof(contacts->numero[count]),stdin);
-            printf("donner  email : ");
-            fgets(contacts->email[count] ,sizeof(contacts->email[count]),stdin);
+           
+            printf("donner nom : ");
+                scanf("%[^\n]",contacts[count].nom);
+                
+                getchar();
+                printf("donner numero : ");
+                scanf("%[^\n]",contacts[count].numero);
+                
+                getchar();
+                printf("donner email : ");
+                scanf("%[^\n]",contacts[count].email);
             count++;
             break;
         case 2:
-           char newEmail[200], newNumero[10];
+           
            printf("donner le nom de cantact qui veux modifer : ");
            fgets(name , sizeof(name),stdin);
            for (int i = 0; i < count; i++)
            {
-            if (strcmp(contacts->nom[i], name)==0)
+            if (strcmp(contacts[i].nom, name)==0)
             {
                 int choix1;
                 do
@@ -47,20 +56,24 @@ int main(){
                 printf("3.quitter\n");
                 printf("votre choix : ");
                 scanf("%d",&choix1);
+                getchar();
                 switch (choix1)
                 {
                 case 1:
                     printf("donner nouveux numero : ");
-                    for (int j = 0; j < 10; j++)
-                    {
-                        fgets(newNumero[j],sizeof(newNumero[j]),stdin);
-                        contacts->numero[i][j] = newNumero[j];
-                    } 
+                    
+                        scanf("%s", &newNumero);
+                        strcpy(contacts[i].numero,newNumero);
+    
                     break;
                 case 2:
+
                     printf("donner nouveux email : ");
                     fgets(newEmail,sizeof(newEmail),stdin);
-                    contacts->email[i]= newEmail;
+
+                    strcpy(contacts[i].email , newEmail);
+
+                    break;
                 default:
                     printf("error");
                     break;
@@ -69,6 +82,8 @@ int main(){
                 
                 
 
+            }else {
+                printf("not found\n");
             }
             
            }
@@ -77,59 +92,60 @@ int main(){
          case 3:
           printf("donner le nom qui veux supprimer : ");
           fgets(name,sizeof(name),stdin);
+
           for (int i = 0; i < count; i++)
           {
-            if (strcasecmp(contacts->nom[i], name)==0)
+            if (strcasecmp(contacts[i].nom, name)==0)
             {
                 for (int j = i; i < count-1; j++)
                 {
-                    contacts->nom[i]=contacts->nom[i+1];
-                    for (int j = 0; i < 10; j++)
-                    {
-                       contacts->numero[i][j]=contacts->numero[i+1][j];
-                    }
-                    contacts->email[i]=contacts->email[i+1];
+                    strcpy(contacts[i].nom ,contacts[j+1].nom);
+                    strcpy(contacts[i].numero ,contacts[j+1].numero);
+                    strcpy(contacts[i].email ,contacts[j+1].email);
                 }
               count--;  
+            }else {
+                printf("not found\n");
             }
             
           }
           
           break;
           case 4:
-          for (int i = 0; i < count; i++)
+          if (count == 0)
           {
-            printf("contact %d",i);
-            printf("nom : %s",contacts->nom[i]);
-            printf("numero : %d",contacts->numero[i]);
-            printf("email : %s",contacts->email[i]);
+            printf("no contact\n");
+          }else{
+             for (int i = 0; i < count; i++)
+          {
+            printf("contact %d\n",i+1);
+            printf("nom : %s\n", contacts[i].nom);
+            printf("numero : %s\n" , contacts[i].numero) ;
+            printf("email : %s\n" , contacts[i].email);
 
           }
+          }
+          
+          
           
           break;
           case 5:
           printf("donner le nom qui veux trouver : ");
           fgets(name,sizeof(name),stdin);
+          int found=0;
           for (int i = 0; i < count; i++)
           {
-            if (strcasecmp(contacts->nom[i], name)==0)
+            
+            if (strcasecmp(contacts[i].nom, name) == 0)
             {
-                for (int j = i; i < count-1; j++)
-                {
-                   printf("nome : %s\n",contacts->nom[i]) ;
-                   printf("numero : ");
-                   for (int a = 0; a < 10; a++)
-                   {
-                    printf("%d",contacts->numero[i][a]);
-                   }
-                   
-                 
-                   printf("email : %s\n",contacts->email[i]) ;
-                   
-                }
+                
+                
+                   printf("nome : %s\n",contacts[i].nom) ;
+                   printf("numero %s\n",contacts[i].numero);
+                   printf("email : %s\n",contacts[i].email) ;
+                   found=1;
+                
                
-            }else {
-                printf("pas trouver\n");
             }
             
           }
